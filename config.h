a3b7af3f2eb20223b3aa0eefae5b47b80305c808
stan <hdstansen@gmail.com>
2022-08-15 11:13:47 +0200

    hardcode new lineheight

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static const char *colors[SchemeLast][2] = {
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 31; // hardcoded dwm-bar height
+static unsigned int lineheight = 49; // HACK: hardcoded to dwm's bar height
 static unsigned int min_lineheight = 8;
 
 /*
